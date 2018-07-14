341d8b21e45e79efd915bc5001107f294c75f19e
Anon Mafia <40897876+YourAnonMafia@users.noreply.github.com>
2018-07-14 10:10:35 -0700

    Update mafiaproject.sh

--- mafiaproject.sh
+++ mafiaproject.sh
@@ -2,17 +2,17 @@
 # The Mafia Project
 
 banner() {
-printf "\n"
-printf "\n               The
-printf "\033[36m         __  ___      _____          ____               _           __ \e[0m\n"
-printf "\033[36m        /  |/  /___ _/ __(_)___ _   / __ \_________    (_)__  _____/ /_    \e[0m\n"
-printf "\033[36m       / /|_/ / __ `/ /_/ / __ `/  / /_/ / ___/ __ \  / / _ \/ ___/ __/ \e[0m\n"
-printf "\033[36m      / /  / / /_/ / __/ / /_/ /  / ____/ /  / /_/ / / /  __/ /__/ /_  \e[0m\n"
-printf "\033[36m     /_/  /_/\__,_/_/ /_/\__,_/  /_/   /_/   \____/_/ /\___/\___/\__/   \e[0m\n"
-printf "\n"                                                                        
-printf "\033[91m      Compiled \e[0m\033[33m by the \e[0m\n\033[91m Anon Mafo \e[0m\n"
-printf "\033[36m       .::.:::  .::  .::     .::      \e[0m\e[1;93m.:::::    \e[0m\n"
-printf "\033[33m   Run the command ls when in the MafiaProject directory to see available tools \e[0m\n"
-printf "\n"
-printf "\033[36m  ###########################################################################\e[0m\n"
-printf "\n"
+print "\n"
+print "\n               The
+print "\033[36m         __  ___      _____          ____               _           __ \e[0m\n"
+print "\033[36m        /  |/  /___ _/ __(_)___ _   / __ \_________    (_)__  _____/ /_    \e[0m\n"
+print "\033[36m       / /|_/ / __ `/ /_/ / __ `/  / /_/ / ___/ __ \  / / _ \/ ___/ __/ \e[0m\n"
+print "\033[36m      / /  / / /_/ / __/ / /_/ /  / ____/ /  / /_/ / / /  __/ /__/ /_  \e[0m\n"
+print "\033[36m     /_/  /_/\__,_/_/ /_/\__,_/  /_/   /_/   \____/_/ /\___/\___/\__/   \e[0m\n"
+print "\n"                                                                        
+print "\033[91m      Compiled \e[0m\033[33m by the \e[0m\n\033[91m Anon Mafo \e[0m\n"
+print "\033[36m       .::.:::  .::  .::     .::      \e[0m\e[1;93m.:::::    \e[0m\n"
+print "\033[33m   Run the command ls when in the MafiaProject directory to see available tools \e[0m\n"
+print "\n"
+print "\033[36m  ###########################################################################\e[0m\n"
+print "\n"
