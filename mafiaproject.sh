0d38c7091f9088366b9e0bc8572fef794ac8872a
Anon Mafia <40897876+YourAnonMafia@users.noreply.github.com>
2018-07-14 10:31:10 -0700

    Update mafiaproject.sh

--- mafiaproject.sh
+++ mafiaproject.sh
@@ -15,13 +15,12 @@ RESET=$(tput sgr0)
 
 banner() {
 printf "${CYAN}              The      
-printf "${CYAN}         __  ___      _____          ____               _           __ \e[0m\n"
-printf "${CYAN}        /  l/  /___ _/ __l_l___ _   / __ \_________    l_l__  _____/ /_    \e[0m\n"
-printf "${CYAN}       / /l_/ / __ `/ /_/ / __ `/  / /_/ / ___/ __ \  / / _ \/ ___/ __/ \e[0m\n"
-printf "${CYAN}      / /  / / /_/ / __/ / /_/ /  / ____/ /  / /_/ / / /  __/ /__/ /_  \e[0m\n"
-printf "${CYAN}     /_/  /_/\__,_/_/ /_/\__,_/  /_/   /_/   \____/_/ /\___/\___/\__/   \e[0m\n"
-printf "${CYAN}                                                 /___/   
-printf "\n"                                                                      
+printf "${CYAN}         __  ___      _____          ____               _           __      ${RESET}"
+printf "${CYAN}        /  l/  /___ _/ __l_l___ _   / __ \_________    l_l__  _____/ /_     ${RESET}"
+printf "${CYAN}       / /l_/ / __ `/ /_/ / __ `/  / /_/ / ___/ __ \  / / _ \/ ___/ __/     ${RESET}"
+printf "${CYAN}      / /  / / /_/ / __/ / /_/ /  / ____/ /  / /_/ / / /  __/ /__/ /_       ${RESET}"
+printf "${CYAN}     /_/  /_/\__,_/_/ /_/\__,_/  /_/   /_/   \____/_/ /\___/\___/\__/       ${RESET}"
+printf "${CYAN}                                                 /___/                                                                     
 printf "${RED}      Compiled ${RESET} by the ${RED} Anon Mafa ${RESET}"
 printf "\n"
 printf "${YELLOW}   Run the command ls when in the MafiaProject directory to see available tools ${RESET}"
